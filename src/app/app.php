b92e86e024f30e3fd231c64722318c500dfeda55
Mrrll <edgarmarrollo@gmail.com>
2021-07-06 18:42:56 +0200

    Reordenando y variables env

--- src/app/app.php
+++ src/app/app.php
@@ -3,13 +3,7 @@
 // *: Importamos las classes necesarias ...
 use DI\Container;
 use Slim\Factory\AppFactory;
-use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
-use Doctrine\Common\Annotations\AnnotationReader;
-use Doctrine\Common\Cache\FilesystemCache;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
-use Doctrine\ORM\Tools\Setup;
 // *: Importamos el Autoload de las classes ...
 require_once __DIR__ . '/../../vendor/autoload.php';
 //* Container de Slim DI ...
@@ -19,46 +13,13 @@ $settings($container); // ?: Añadimos al contenedor ...
 // *: Crear App con la Fabrica Slim ...
 AppFactory::setContainer($container); // ?: Añadimos a la aplicacion ...
 $app = AppFactory::create(); // ?: Creamos la instancia app ...
-
-// *: Agregar servicio de Doctrine a su contenedor ...
-$container->set(EntityManager::class, static function (
-    Container $container
-): EntityManager {
-    $settings = $container->get('settings');
-
-    $isDevMode = true;
-    $proxyDir = null;
-    $cache = null;
-    $useSimpleAnnotationReader = false;
-    $config = Setup::createAnnotationMetadataConfiguration(
-        [__DIR__ . '/src'],
-        $isDevMode,
-        $proxyDir,
-        $cache,
-        $useSimpleAnnotationReader
-    );
-
-    return EntityManager::create($settings['doctrine']['connection'], $config);
-});
-// *: Agregar servicio de vista a su contenedor ...
-$container->set('view', function ($container) {
-    // *: Motor de Plantilla Twig ...
-    return Twig::create(__DIR__ . '/../resources/Views', ['cache' => false]);
-});
+// *: Importamos los servicios del contenedor ...
+require_once __DIR__ . './container.php';
 // *: Añadir Twig-View Middleware ...
 $app->add(TwigMiddleware::create($app, $container->get('view')));
-// *: Agregar servicio de IndexController a su contenedor ...
-$container->set('IndexController', function ($container) {
-    return new \App\Controllers\IndexController($container);
-});
-$container->set('RegisterController', function ($container) {
-    return new \App\Controllers\Auth\RegisterController($container);
-});
 // *: Middleware salida 404 ...
 $middleware = require_once __DIR__ . './Middleware/Middleware.php';
 $middleware($app);
 // *: Rutas ...
 $routes = require_once __DIR__ . '/../resources/Routes/web.php';
 $routes($app);
-// *: Arrancamos la app ...
-$app->run();
---
+++ src/app/container.php
@@ -0,0 +1,39 @@
+<?php
+// TODO: Archivo de Configuracion de los Servicios del contenedor Slim ...
+// *: Importamos las classes necesarias ...
+use DI\Container;
+use Slim\Views\Twig;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+// *: Agregar servicio de Doctrine a su contenedor ...
+$container->set(EntityManager::class, static function (
+    Container $container
+): EntityManager {
+    $settings = $container->get('settings');
+
+    $isDevMode = true;
+    $proxyDir = null;
+    $cache = null;
+    $useSimpleAnnotationReader = false;
+    $config = Setup::createAnnotationMetadataConfiguration(
+        [__DIR__ . './src'],
+        $isDevMode,
+        $proxyDir,
+        $cache,
+        $useSimpleAnnotationReader
+    );
+
+    return EntityManager::create($settings['doctrine']['connection'], $config);
+});
+// *: Agregar servicio de vista a su contenedor ...
+$container->set('view', function ($container) {
+    // *: Motor de Plantilla Twig ...
+    return Twig::create(__DIR__ . '/../resources/Views', ['cache' => false]);
+});
+// *: Agregar servicio de IndexController a su contenedor ...
+$container->set('IndexController', function ($container) {
+    return new \App\Controllers\IndexController($container);
+});
+$container->set('RegisterController', function ($container) {
+    return new \App\Controllers\Auth\RegisterController($container);
+});
