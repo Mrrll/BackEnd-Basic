469dab641754b8b737590dfe3dcffc02966ba6ee
Mrrll <edgarmarrollo@gmail.com>
2021-07-15 00:04:58 +0200

    Proteccion de rutas

--- src/app/Middleware/GuestMiddleware.php
+++ src/app/Middleware/GuestMiddleware.php
@@ -1,13 +1,13 @@
 <?php
 namespace App\Middleware;
-// TODO: Archivo que gestiona el permisos hacia las rutas si no hay sesion abierta ...
+// TODO: Archivo que gestiona el permisos hacia las rutas si hay sesion abierta ...
 // *: Importamos las classes necesarias ...
 use App\Controllers\Controller;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Routing\RouteContext;
-class AuthMiddleware extends Controller
+class GuestMiddleware extends Controller
 {
     public function __invoke(Request $request, RequestHandler $handler) : Response
     {
@@ -16,10 +16,10 @@ class AuthMiddleware extends Controller
         // ! -------------------------------------------------------------------
          // *: Compruebe si el usuario no ha iniciado sesión ...
          $response = $handler->handle($request);
-        if (!$this->auth->check()) {
+        if ($this->auth->check()) {
             // *: Mensaje alert desde flash slim ...
-            $this->flash->addMessage('error', 'Please sign in before doing that.');
-            return $response->withHeader('Location',  $routes->urlFor('auth.login'));// ?: Redireccionamos a la plantilla ...
+            $this->flash->addMessage('info', 'There is already a login open.');
+            return $response->withHeader('Location',  $routes->urlFor('home'));// ?: Redireccionamos a la plantilla ...
         }
         return $response;
     }
--- src/resources/Routes/web.php
+++ src/resources/Routes/web.php
@@ -5,29 +5,36 @@ use Slim\App;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Middleware\AuthMiddleware;
+use App\Middleware\GuestMiddleware;
 // *: Creamos rutas ...
-return function (App $app)
-{
+return function (App $app) {
     // *: Rutas web ...
     $app->get('/', 'IndexController:index')->setName('welcom');
 
-    $app->get('/home', 'HomeController:index')->setName('home');
-
-
-    // *: Ruta del registro ...
-    $app->get('/register', 'RegisterController:index')->setName('auth.register');
-    $app->post('/register', 'RegisterController:register');
-    // *: Ruta del login ...
-    $app->get('/login', 'LoginController:index')->setName('auth.login');
-    $app->post('/login', 'LoginController:login');
-
+    $app->group('guest', function () use ($app) {
+        // *: Ruta del registro ...
+        $app
+            ->get('/register', 'RegisterController:index')
+            ->setName('auth.register');
+        $app->post('/register', 'RegisterController:register');
+        // *: Ruta del login ...
+        $app->get('/login', 'LoginController:index')->setName('auth.login');
+        $app->post('/login', 'LoginController:login');
+    })->add(new GuestMiddleware($app->getContainer()));
     // *: Rutas Auth ...
-    $app->group('auth', function () use ($app)
-    {
-        // *: Ruta del logout ...
-    $app->get('/logout', 'LoginController:logout')->setName('auth.logout');
-    // *: Ruta del Cambio password ...
-    $app->get('/change', 'PasswordController:index')->setName('auth.password.change');
-    $app->post('/change', 'PasswordController:ChangePassword');
-    })->add(new AuthMiddleware($app->getContainer()));
+    $app
+        ->group('auth', function () use ($app) {
+            // *: Ruta web del usuario ...
+            $app->get('/home', 'HomeController:index')->setName('home');
+            // *: Ruta del logout ...
+            $app
+                ->get('/logout', 'LoginController:logout')
+                ->setName('auth.logout');
+            // *: Ruta del Cambio password ...
+            $app
+                ->get('/change', 'PasswordController:index')
+                ->setName('auth.password.change');
+            $app->post('/change', 'PasswordController:ChangePassword');
+        })
+        ->add(new AuthMiddleware($app->getContainer()));
 }; // ?: funcion de retorno y solicitud de la App ...
