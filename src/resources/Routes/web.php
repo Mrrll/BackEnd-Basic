e5356ce0aa54e447b84816df15b3dd6874d9ab9f
Mrrll <edgarmarrollo@gmail.com>
2021-07-14 23:49:16 +0200

    Permisos hacia las rutas si hay sesion

--- src/resources/Routes/web.php
+++ src/resources/Routes/web.php
@@ -4,6 +4,7 @@
 use Slim\App;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Middleware\AuthMiddleware;
 // *: Creamos rutas ...
 return function (App $app)
 {
@@ -12,16 +13,21 @@ return function (App $app)
 
     $app->get('/home', 'HomeController:index')->setName('home');
 
-    // *: Rutas Auth ...
+
     // *: Ruta del registro ...
     $app->get('/register', 'RegisterController:index')->setName('auth.register');
     $app->post('/register', 'RegisterController:register');
     // *: Ruta del login ...
     $app->get('/login', 'LoginController:index')->setName('auth.login');
     $app->post('/login', 'LoginController:login');
-    // *: Ruta del logout ...
+
+    // *: Rutas Auth ...
+    $app->group('auth', function () use ($app)
+    {
+        // *: Ruta del logout ...
     $app->get('/logout', 'LoginController:logout')->setName('auth.logout');
     // *: Ruta del Cambio password ...
     $app->get('/change', 'PasswordController:index')->setName('auth.password.change');
     $app->post('/change', 'PasswordController:ChangePassword');
+    })->add(new AuthMiddleware($app->getContainer()));
 }; // ?: funcion de retorno y solicitud de la App ...
