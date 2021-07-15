662cf68e58fcdc85ff9edd1030e2a20465fcf893
Mrrll <edgarmarrollo@gmail.com>
2021-07-16 00:08:20 +0200

    Nuevo campo de verificacion de email

---
+++ src/database/migrations/Version20210715215336.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210715215336 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Añadimos campo a tabla';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $Table =  $schema->getTable('usuarios');
+        $Table->addColumn("email_verified_at", "datetime", array("notnull" => false));
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE usuraios DROP COLUMN email_verified_at;');
+    }
+}
